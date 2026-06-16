f2c44feb9ef1f89a927cf88a59b2c028971d8ab8
Rajabali321 <rajabaliofficial60@gmail.com>
2026-06-16 12:14:57 +0500

    Add app/src/main/java/com/voiceai3/BootReceiver.kt

---
+++ app/src/main/java/com/voiceai3/BootReceiver.kt
@@ -0,0 +1,18 @@
+package com.voiceai3
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            if (VoiceAccessibilityService.instance != null) {
+                val launchIntent = Intent(context, MainActivity::class.java).apply {
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                }
+                context.startActivity(launchIntent)
+            }
+        }
+    }
+}
